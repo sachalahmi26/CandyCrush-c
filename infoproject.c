7dc7339796c123930055454b2720cea9f0dcda7d
sachalahmi26 <sachalahmi26@gmail.com>
2025-12-05 13:24:56 +0100

    Update infoproject.c

--- infoproject.c
+++ infoproject.c
@@ -5,38 +5,6 @@
 #define N 25
 #define P 45
 
-//Sous programme qui affiche le menu
-int affichermenu(){
-    int choix;
-    printf("1. Lire les regles du jeu\n");
-    printf("2. Commencer une nouvelle partie (à partir du 1er niveau)\n");
-    printf("3. Reprendre une partie (au dernier niveau enregistre)\n");
-    printf("4. Quitter\n");
-    printf("Votre choix : ");
-    scanf("%d", &choix);
-    if(choix==1){
-        printf("Il y a trois niveaux de difficultes croissantes\n");
-        printf("Vous avez un nombre de vies attibuee qui est limite\n");
-        printf("Quand vous perdez un niveau vous perdez une vie\n");
-        printf("Vous devez eliminer un grand nombre d'items en un nombre maximum de coups et en un temps limite\n");
-        return 0;
-    }
-    else if(choix==2){
-        printf("Vous avez commence une nouvelle partie\n");
-        srand(time(NULL));
-        int nb_vies = rand()%21;
-        printf("Nombre de vies : %d", nb_vies);
-    }
-    else if(choix==4){
-        printf("Vous avez quitte\n");
-        return 0;
-    }
-    else if(choix<1||choix>4){
-        printf("Erreur vous devez saisir un choix\n");
-        return 0;
-    }
-}
-
 //Sous programme qui remplit le tableau par des items
 void remplir(char tab[N][P]){
     char items[]={'O','X','#','$','*'};
@@ -58,10 +26,103 @@ void affichergrille(char tab[N][P]){
     }
 }
 
+//Sous programme qui efface les items
+void effacer(char tab[N][P]){
+    int aEffacer[N][P] = {0};
+    for(int i=0;i<N;i++){
+        for(int j=0;j<P-2;j++){
+            char v = tab[i][j];
+            if (v != ' ' &&
+                tab[i][j+1] == v &&
+                tab[i][j+2] == v &&
+                tab[i][j+3] == v) {
+                aEffacer[i][j] = 1;
+                aEffacer[i][j+1] = 1;
+                aEffacer[i][j+2] = 1;
+                aEffacer[i][j+3] = 1;
+            }
+        }
+    }
+    for(int j=0;j<P;j++){
+        for(int i=0;i<N-2;i++){
+            char v = tab[i][j];
+            if (v != ' ' &&
+                tab[i+1][j] == v &&
+                tab[i+2][j] == v &&
+                tab[i+3][j] == v) {
+                aEffacer[i][j] = 1;
+                aEffacer[i+1][j] = 1;
+                aEffacer[i+2][j] = 1;
+                aEffacer[i+3][j] = 1;
+            }
+        }
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j<P;j++){
+            if(aEffacer[i][j] == 1){
+                tab[i][j] = ' ';
+            }
+        }
+    }
+}
+
+void gravite(char tab[N][P]) {
+    for (int j = 0; j < P; j++) {
+        int bas = N - 1;
+        for (int i = N - 1; i >= 0; i--) {
+            if (tab[i][j] != ' ') {
+                tab[bas][j] = tab[i][j];
+                if (bas != i) {
+                    tab[i][j] = ' ';
+                }
+                bas--;
+            }
+        }
+        while (bas >= 0) {
+            char items[]={'O','X','#','$','*'};
+            int nbItems = 5;
+            tab[bas][j] = items[rand()%nbItems];   // Nouvel item aléatoire
+            bas--;
+        }
+    }
+}
+
 int main(){
+    int choix;
     char tab[N][P];
-    srand(time(NULL));
-    remplir(tab);
-    affichergrille(tab);
-    return 0;
+    printf("1. Lire les regles du jeu\n");
+    printf("2. Commencer une nouvelle partie (A partir du 1er niveau)\n");
+    printf("3. Reprendre une partie (au dernier niveau enregistre)\n");
+    printf("4. Quitter\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+    if(choix==1){
+        printf("Il y a trois niveaux de difficultes croissantes\n");
+        printf("Vous avez un nombre de vies attibuee qui est limite\n");
+        printf("Quand vous perdez un niveau vous perdez une vie\n");
+        printf("Vous devez eliminer un grand nombre d'items en un nombre maximum de coups et en un temps limite\n");
+        return 0;
+    }
+    else if(choix==2){
+        printf("Vous avez commence une nouvelle partie\n");
+        srand(time(NULL));
+        int nb_vies = rand()%21;
+        printf("Nombre de vies : %d\n", nb_vies);
+        srand(time(NULL));
+        remplir(tab);
+        affichergrille(tab);
+        printf("\n");
+        effacer(tab);
+        gravite(tab);
+        affichergrille(tab);
+        return 0;
+    }
+    else if(choix==4){
+        printf("Vous avez quitte\n");
+        return 0;
+    }
+    while(choix<1||choix>4){
+        printf("Erreur vous devez saisir un choix\n");
+        return main();
+    }
 }
